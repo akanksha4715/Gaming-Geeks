25681811b944314849b7196cce4c14585d2cb939
Akanksha Tomar <akanksha4715@gmail.com>
2020-11-10 02:20:59 +0530

    queryParams

--- controllers/logincon.js
+++ controllers/logincon.js
@@ -8,7 +8,8 @@ const customers=[];
 const p = path.join(path.dirname(process.mainModule.filename),'data','customers.json'); //process.mainModule.filename will return the location of the main file which run the server.
 
 exports.addlogindetails=(req,res,next)=>{
-        customers.push({Id : req.body.uname, Age : req.body.age});
+        
+        customers.push({Game: req.query.gameDetails, Id : req.body.uname, Age : req.body.age});
         console.log(customers);
         fs.writeFile(p, JSON.stringify(customers),err=>{
             console.log(err);
