51b9cc5ebf318ba02838a782031ceea10c284a17
Akanksha Tomar <akanksha4715@gmail.com>
2020-11-10 00:44:14 +0530

    data

--- controllers/logincon.js
+++ controllers/logincon.js
@@ -1,12 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 exports.getloginpage=((req,res,next)=>{
     res.render('login');
     });
 
-    const customers=[];
+const customers=[];
+const p = path.join(path.dirname(process.mainModule.filename),'data','customers.json'); //process.mainModule.filename will return the location of the main file which run the server.
 
 exports.addlogindetails=(req,res,next)=>{
-        customers.push({Id:req.body.uname,
-                        Age : req.body.age});
-                        console.log(customers);
-                        res.redirect('/home');
+        customers.push({Id : req.body.uname, Age : req.body.age});
+        console.log(customers);
+        fs.writeFile(p, JSON.stringify(customers),err=>{
+            console.log(err);
+        });
+        res.redirect('/home');
 };
