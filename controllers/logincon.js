c2c2ba7e11dd2ac79bd45e3928257769852e6cf8
Akanksha Tomar <akanksha4715@gmail.com>
2020-11-19 16:19:01 +0530

    db

--- controllers/logincon.js
+++ controllers/logincon.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-exports.getloginpage=((req,res,next)=>{
-    res.render('login');
-    });
+const url = require('url');
 
 const customers=[];
 const p = path.join(path.dirname(process.mainModule.filename),'data','customers.json'); //process.mainModule.filename will return the location of the main file which run the server.
 
+exports.getloginpage=((req,res,next)=>{
+    res.render('login');
+    });
+
 exports.addlogindetails=(req,res,next)=>{
-        
-        customers.push({Game: req.query.gameDetails, Id : req.body.uname, Age : req.body.age});
+        const querystrin =url.parse(req.url,true).search;
+        customers.push({Game: req.query, Id : req.body.uname, Age : req.body.age});
         console.log(customers);
+        console.log(querystrin);
         fs.writeFile(p, JSON.stringify(customers),err=>{
             console.log(err);
         });
